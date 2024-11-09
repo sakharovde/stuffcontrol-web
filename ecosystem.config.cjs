01f1bd11e07373a46c215cb69b4a067f4fd8e061
Denis Sakharov <denis.sakharov@icloud.com>
2024-11-09 21:50:41 +0400

    Add prettier

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-    apps: [
-        {
-            name: 'Vite',
-            script: 'yarn vite',
-        },
-        {
-            name: 'TypesScript',
-            script:
-                'yarn tsc --noEmit --watch --preserveWatchOutput --skipLibCheck --project tsconfig.app.json',
-        },
-    ],
+  apps: [
+    {
+      name: 'Vite',
+      script: 'yarn vite',
+    },
+    {
+      name: 'TypesScript',
+      script:
+        'yarn tsc --noEmit --watch --preserveWatchOutput --skipLibCheck --project tsconfig.app.json',
+    },
+  ],
 };
