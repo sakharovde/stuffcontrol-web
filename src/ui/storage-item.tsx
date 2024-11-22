45d5879167b6ec6f0c4b543659b917de024f65c4
Denis Sakharov <denis.sakharov@icloud.com>
2024-11-22 16:16:22 +0400

    Show product name in UI

--- src/ui/storage-item.tsx
+++ src/ui/storage-item.tsx
@@ -1,14 +1,26 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import StorageItemModel from '../core/domain/models/storage-item.ts';
+import { useQuery } from '@tanstack/react-query';
+import CoreContext from './core-context.ts';
 
 type Props = {
   data: StorageItemModel;
 };
 
 const StorageItem: FC<Props> = (props) => {
+  const core = useContext(CoreContext);
+  const productQuery = useQuery({
+    queryKey: ['products', props.data.productId],
+    queryFn: () => core.useCases.product.get.execute(props.data.productId),
+  });
+
+  if (!productQuery.data) {
+    return null;
+  }
+
   return (
     <div className='flex justify-between'>
-      <div>{props.data.id}</div>
+      <div>{productQuery.data.name}</div>
       <div>{props.data.quantity}</div>
     </div>
   );
