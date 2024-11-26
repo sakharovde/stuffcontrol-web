bb954f72eb88d972702a8e84ec15d82ba2c8b9de
Denis Sakharov <denis.sakharov@icloud.com>
2024-11-26 04:25:31 +0400

    Add some info for manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,28 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Stuff Control',
+        short_name: 'Stuff',
+        theme_color: '#000000',
+
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 });
