7ac950a1ae8744c2cbb26dbc0a078b36a13b0e3a
Denis Sakharov <denis.sakharov@icloud.com>
2025-08-31 19:37:05 +0500

    Disable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      // devOptions: {
-      //   enabled: process.env.NODE_ENV !== 'production',
-      // },
-      manifest: {
-        name: 'Stuff Control',
-        short_name: 'Stuff',
-        theme_color: '#000000',
-
-        icons: [
-          {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
-          },
-          {
-            src: '/vite.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   // devOptions: {
+    //   //   enabled: process.env.NODE_ENV !== 'production',
+    //   // },
+    //   manifest: {
+    //     name: 'Stuff Control',
+    //     short_name: 'Stuff',
+    //     theme_color: '#000000',
+    //
+    //     icons: [
+    //       {
+    //         src: '/vite.svg',
+    //         sizes: '192x192',
+    //         type: 'image/svg+xml',
+    //       },
+    //       {
+    //         src: '/vite.svg',
+    //         sizes: '512x512',
+    //         type: 'image/svg+xml',
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   server: {
     proxy: {
