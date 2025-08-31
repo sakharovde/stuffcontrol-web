a04a5d770ccca637c250042cb376992966580497
Denis Sakharov <denis.sakharov@icloud.com>
2025-09-01 00:42:02 +0500

    Change storage list to antd

--- src/ui/widgets/storages-widget.tsx
+++ src/ui/widgets/storages-widget.tsx
@@ -1,21 +1,8 @@
-import { FC, useContext, useLayoutEffect, useState } from 'react';
+import { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { StorageDto, BatchDto } from '../../application';
-import CoreContext from '../core-context.ts';
-import { SafeArea } from 'antd-mobile';
-
-type StorageItemProps = {
-  data: BatchDto;
-};
-const StorageProductWidget: FC<StorageItemProps> = (props) => {
-  return (
-    <div className='flex text-xs justify-between text-gray-400'>
-      <div>{props.data.name}</div>
-      <div>{props.data.quantity}</div>
-    </div>
-  );
-};
+import { StorageDto } from '../../application';
+import { List, SafeArea } from 'antd-mobile';
 
 type StorageCardWidgetProps = {
   data: StorageDto;
@@ -23,42 +10,7 @@ type StorageCardWidgetProps = {
 };
 
 const StorageCardWidget: FC<StorageCardWidgetProps> = (props) => {
-  const core = useContext(CoreContext);
-  const [products, setProducts] = useState<BatchDto[]>([]);
-
-  useLayoutEffect(() => {
-    const updateProsuctsState = () => {
-      core.queries.batch.getByStorage({ storageId: props.data.id }).then(setProducts);
-    };
-
-    core.events.product.on('productCreated', updateProsuctsState);
-    core.events.product.on('productUpdated', updateProsuctsState);
-    core.events.batch.on('batchCreated', updateProsuctsState);
-    core.events.batch.on('batchUpdated', updateProsuctsState);
-
-    updateProsuctsState();
-
-    return () => {
-      core.events.product.off('productCreated', updateProsuctsState);
-      core.events.product.off('productUpdated', updateProsuctsState);
-      core.events.batch.off('batchCreated', updateProsuctsState);
-      core.events.batch.off('batchUpdated', updateProsuctsState);
-    };
-  }, [core]);
-
-  return (
-    <div className='border rounded-md p-3 cursor-pointer' onClick={props.onClick}>
-      <div className='font-semibold'>{props.data.name}</div>
-      {!products.length && <div className='text-gray-400'>Empty</div>}
-      {!!products.length && (
-        <div>
-          {products.map((product) => (
-            <StorageProductWidget key={product.id} data={product} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <List.Item onClick={props.onClick}>{props.data.name}</List.Item>;
 };
 
 type StoragesWidgetProps = {
@@ -71,11 +23,11 @@ const StoragesWidget: FC<StoragesWidgetProps> = (props) => {
   return (
     <>
       <div className='p-3'>
-        <div className='flex gap-5 flex-col'>
+        <List className='flex gap-5 flex-col'>
           {props.data?.map((storage) => (
             <StorageCardWidget key={storage.id} data={storage} onClick={() => props.onClickStorageCard(storage.id)} />
           ))}
-        </div>
+        </List>
       </div>
       <div className='absolute bottom-0 left-0  bg-gray-100 w-full'>
         <div className='h-10 flex items-center px-3'>
